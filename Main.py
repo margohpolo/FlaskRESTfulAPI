8ece0df7c3e0ee4661de929a106b9c2a267c15d0
Wei Zheng <wzyeo1990@gmail.com>
2020-02-12 19:53:36 +0800

    Saved predictions

--- Main.py
+++ Main.py
@@ -43,19 +43,36 @@ class Main(Resource):
     #return {getDict_id: latestPrediction}, 200
     return "hello"
 
-  def GenerateModel(dictionary):
-    return Prediction.GenerateModel(dictionary)
+  def GenerateModel(dictionary, id):
+    demand = ProcessData.ProcessData(dictionary)
+    Prediction.GenerateModel(dictionary, demand)
+    latestPrediction = Prediction.GetPrediction(dictionary, demand)
+    loadDict[id] = latestPrediction
+    with open("savedPredictions.txt", "a+") as outfile:
+      json.dump({"id": id, "predictions": latestPrediction}, outfile)
+      outfile.write("\n")
+    print("latestPrediction type: ", type(latestPrediction), "\n latestPrediction: ", latestPrediction)
+    return "Model Generation completed."
 
   def GetPrediction(dictionary):
     return Prediction.GetPrediction(dictionary)
   
   @app.route("/get")
   def get():
-    getDict_id = request.args.get('id')
-    dictionary = request.args.get('data')
-    loadDict = json.loads(dictionary.replace('\'', '\"'))
-    return {getDict_id: Main.GetPrediction(loadDict)}, 200
-    #return {latestPrediction}, 200
+    get_id = request.args.get('id')
+    found = False
+    infile = open("savedPredictions.txt", "r")
+    lines = infile.readlines()
+    for line in lines:
+      data = json.loads(line)
+      if(data["id"] == get_id):
+        found = True
+        predictions = data["predictions"]
+    if found:
+      return {"predictions": predictions, "result": "success"}, 200
+    else:
+      return {"result": "failed"}, 200
+#    return {get_id: (loadDict[get_id]).replace("\\","")}, 200 <- this is not working :()
   
 api.add_resource(Main, '/<string:getDict_id>')
 
